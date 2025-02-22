4a2ff05e4dd2bbdb1d2187b2988f90000cf91ca6
AlbinS35 <albinsuresh1875@gmail.com>
2025-02-22 17:38:54 +0530

    template integrated

--- Car/Car/settings.py
+++ Car/Car/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,7 +118,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+STATIC_URL = '/static/'
+
+# For production - Collect all static files here
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# For development - Serve additional static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
