2b90fbb0102d5fa579f2b0fe4db5ddfe3960abb9
AlbinS35 <albinsuresh1875@gmail.com>
2025-02-22 18:48:41 +0530

    integrated the template , 22/02/25, commited by abimel

--- Car/Car/settings.py
+++ Car/Car/settings.py
@@ -127,9 +127,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # For development - Serve additional static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 # Default primary key field type
