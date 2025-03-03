e3a9c98cf8fa3e954984b5429b3ff93664ab41d9
Aromal200 <aromalkremesh5120@gamil.com>
2025-03-03 14:03:40 +0530

    login and signup

--- Car/reviews/urls.py
+++ Car/reviews/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from . import views 
+from .views import login_view,signup_view
 
 urlpatterns = [
     path('', views.index, name='index'),  # Homepage with featured & latest cars
      # Newest cars page
+      path('login/', login_view, name='login'),#login page 
+      path('signup/', signup_view, name='signup'),#signup page
 ]
--- Car/reviews/views.py
+++ Car/reviews/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Car
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -13,3 +16,42 @@ def index(request):
     return render(request, 'index.html', {'latest_cars': latest_cars , 'featured_cars': featured_cars})
 
 
+def login_view(request):   
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')  # Redirect to a home page after login
+            else:
+                # Handle invalid login
+                error_message = 'Invalid login credentials'
+                return render(request, 'login.html', {'form': form, 'error_message': error_message})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            error_message = 'Passwords do not match'
+        elif len(password) > 8:
+            error_message = 'Password not execed than 8 character'
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            login(request, user)
+            return redirect('login')  # Redirect to the login page after signup
+
+        return render(request, 'signup.html', {'error_message': error_message})
+
+    return render(request, 'signup.html')
