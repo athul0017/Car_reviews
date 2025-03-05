f9dbe6afe7f4e374ee24a02fda86535cce073c18
Aromal200 <aromalkremesh5120@gamil.com>
2025-03-05 13:18:21 +0530

    link login page, logout page

--- Car/reviews/urls.py
+++ Car/reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views 
 from .views import login_view,signup_view
+from django.contrib.auth import views as auth_views #auth views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Homepage with featured & latest cars
@@ -8,4 +9,5 @@ urlpatterns = [
       path('login/', login_view, name='login'),#login page 
       path('signup/', signup_view, name='signup'),#signup page
       path('cars/', views.car_listing, name='car_listing'), 
+      path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),  # Logout page
 ]
